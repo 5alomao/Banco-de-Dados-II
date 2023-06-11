9f6a09555ba547c6ebfe606666403e184f4373c1
5alomao <salomao.junior2k22@gmail.com>
2023-06-11 12:17:17 -0300

    Trigger para definir PrecoVenda

---
+++ PetShop/Trigger/produto/tri_FuncPrecoVenda.sql
@@ -0,0 +1,19 @@
+DELIMITER $
+CREATE TRIGGER tri_FuncPrecoVenda
+BEFORE UPDATE 
+ON produto
+FOR EACH ROW
+BEGIN
+	
+	if(NEW.precoCusto != OLD.precoVenda AND NEW.precoCusto > 0) 
+		then
+			SET NEW.precoVenda = NEW.precoCusto + (NEW.precoCusto * NEW.margemLucro/100);
+	END if;
+	
+	if(NEW.margemLucro != OLD.margemLucro AND NEW.margemLucro >= 0) 
+		then
+			SET NEW.precoVenda = NEW.precoCusto + (NEW.precoCusto * NEW.margemLucro/100);
+	END if;
+	
+END$
+DELIMITER ;
